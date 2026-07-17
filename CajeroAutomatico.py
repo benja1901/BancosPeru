79ff210736c5256847d97ce3672335f7b276fa2b
benja1901 <bsalcedomiranda@gmail.com>
2026-07-17 18:47:25 -0500

    Update ATM program with new PIN and balance
    Codigo modificado para Interbank
    Autor: Benjamin Salcedo

--- CajeroAutomatico.py
+++ CajeroAutomatico.py
@@ -1,16 +1,16 @@
 # ===========================================
 # PROGRAMA: CAJERO AUTOMÁTICO
-# Autor: (Benjamin Salcedo)
+# Autor: Benjamin Salcedo 3sec.
 # ===========================================
 
-saldo = 5000
-pin_correcto = "5678"
+saldo = 20000
+pin_correcto = "9012"
 
 print("===================================")
-print("     BIENVENIDO AL CAJERO DEL BCP")
+print("     CAJERO BANCO INTERBAK")
 print("===================================")
 
-pin = input("Ingrese su clave PIN: ")
+pin = input("Ingresar PIN: ")
 
 if pin == pin_correcto:
 
